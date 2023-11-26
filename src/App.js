563392d2d3aba106c0daa6bafa38037d8d600096
Lazerick Lever <lazericklever@Lazericks-MacBook-Pro-2.local>
2023-11-25 21:31:12 -0700

    fresh clean app.js component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 function App() {
 
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  }
-
-  const options = [
-    { label: 'The Color Red', value: 'red'},
-    { label: 'The Color Green', value: 'green'},
-    { label: 'A Shade of Blue', value: 'blue'}
-  ];
-
-  return (
-    <div>
-      <Dropdown 
-        options={options} 
-        value={selection} 
-        onChange={handleSelect} 
-      />
-    </div>
-  )
-}
-
+  return <div>App component</div>
+};
 
 export default App;
 
