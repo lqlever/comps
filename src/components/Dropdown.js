960695d4e13eaf6d4faa8f36f928baac86c2728d
Lazerick Lever <lazericklever@Lazericks-MacBook-Pro-2.local>
2023-11-25 20:50:41 -0700

    finised dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,15 +1,36 @@
+import { useState, useEffect, useRef } from 'react';
 import { GoChevronDown } from 'react-icons/go';
-import { useState } from 'react';
 import Panel from './Panel';
 
 function Dropdown({ options, value, onChange }) {
 
   const [isOpen, setIsOpen] = useState(false);
 
+  const divEL = useRef(); // short for div element
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEL.current) {
+        return;
+      };
+      
+      if (!divEL.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener('click', handler, true);
+
+    return () => {
+      document.removeEventListener('click', handler);
+    }
+ 
+  }, []);
+
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
-
+ 
   const handleOptionClick = (option) => {
     setIsOpen(false);
     onChange(option);
@@ -20,7 +41,7 @@ function Dropdown({ options, value, onChange }) {
   });
 
   return (
-    <div className="w-48 relative">
+    <div ref={divEL} className="w-48 relative">
       <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>{value?.label || 'Select...'} <GoChevronDown className="text-lg" /></Panel>
       {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
     </div>
