1fdf01acf8094f96015dbff3b1ab287fa7cab2b1
Lazerick Lever <lazericklever@Lazericks-MacBook-Pro-2.local>
2023-11-26 16:37:53 -0700

    add route component and link component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import Link from './components/Link';
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+
 function App() {
 
-  return <div>App component</div>
+  return <div>
+    <Link to="/accordion">Go to Accordion</Link>
+    <Link to="/dropdown">Go to Dropdown</Link>
+    <div>
+      <Route path="/accordion">
+        <AccordionPage />
+      </Route>
+      <Route path="/dropdown">
+        <DropdownPage />
+      </Route>
+    </div>
+  </div>
 };
 
 export default App;
