19992a813b44a9ef552254202b33111301af9007
Lazerick Lever <lazericklever@Lazericks-MacBook-Pro-2.local>
2023-11-26 17:10:57 -0700

    add styling and new component for nav

---
+++ src/components/Sidebar.js
@@ -0,0 +1,21 @@
+import Link from "./Link";
+
+function Sidebar() {
+  const links = [
+    { path: "/", label: "Dropdown" },
+    { path: "/accordion", label: "Accordion" },
+    { path: "/buttons", label: "Buttons" }
+  ];
+
+  const renderedLinks = links.map((link) => {
+    return <Link key={link.label} to={link.path} className="mb-3" activeClassName="font-bold border-l-4 border-blue-500 pl-2">{link.label}</Link>
+  });
+
+  return (
+  <div className="sticky top-0 flex flex-col items-start">
+    {renderedLinks}
+  </div>
+  );
+};
+
+export default Sidebar;
