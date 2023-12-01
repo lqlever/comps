0882c8e65cfc64b2e87147596b5dc85c47d3e370
Lazerick Lever <lazerick.dev@gmail.com>
2023-12-01 10:13:52 -0700

    add sample data and content for the table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,34 @@
-function Table({ data }) {
-  return <div>{data.length}</div>
+function Table({ data, config, keyFn }) {
+
+  const renderedRows = data.map((rowData) => {
+
+    const renderedCells = config.map((column) => {
+      return <td key={column.label} className="p-3">{column.render(rowData)}</td>
+    });
+
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  const renderedHeaders = config.map((column) => { 
+    return <th key={column.label} className="p-3">{column.label}</th>
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          {renderedHeaders} 
+        </tr>
+      </thead>
+      <tbody>
+        {renderedRows}
+      </tbody>
+    </table>
+  );
 };
 
 
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -4,13 +4,34 @@ const data = [
   { name: 'Apple', color: 'bg-red-500', score: 5 },
   { name: 'Banana', color: 'bg-yellow-500', score: 3 },
   { name: 'Grape', color: 'bg-purple-500', score: 1 },
-  { name: 'Orange', color: 'bg-orange-500', score: 4 }
+  { name: 'Orange', color: 'bg-orange-500', score: 4 },
+  { name: 'Lime', color: 'bg-green-500', score: 2 }
 ];
+
+const config = [
+  { 
+    label: 'Name',
+    render: (fruit) => fruit.name,
+  },
+  { 
+    label: 'Color',
+    render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>
+  },
+  { 
+    label: 'Score',
+    render: (fruit) => fruit.score
+  } 
+];
+
+const keyFn = (fruit) => {
+  return fruit.name;
+};
+
 
 function TablePage() {
   return (
     <div>
-      <Table data={data} />
+      <Table data={data} config={config} keyFn={keyFn}  />
     </div>
   );
 };
